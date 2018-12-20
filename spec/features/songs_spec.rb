2f34c8df712e5f602821324b84c65096443fec9c
Travis T <travistokuyama@gmail.com>
2018-12-20 16:13:46 +0000

    Automatically backed up by Learn

--- spec/features/songs_spec.rb
+++ spec/features/songs_spec.rb
@@ -1,41 +1,41 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe "songs", type:  :feature do
+describe "songs", type:  :feature do
 
-#   before do
-#     Artist.destroy_all
-#     Song.destroy_all
-#     @artist = Artist.create!(name: "Daft Punk")
-#     @song = @artist.songs.create!(title: "The Grid")
-#   end
+  before do
+    Artist.destroy_all
+    Song.destroy_all
+    @artist = Artist.create!(name: "Daft Punk")
+    @song = @artist.songs.create!(title: "The Grid")
+  end
 
-#   describe "/songs/:id" do
+  describe "/songs/:id" do
 
-#     it "links to the artist" do
-#       visit song_path(@song)
-#       expect(page).to have_link("Daft Punk", href: artist_path(@artist))
-#     end
+    it "links to the artist" do
+      visit song_path(@song)
+      expect(page).to have_link("Daft Punk", href: artist_path(@artist))
+    end
 
-#     it "links to edit when no artist" do
-#       song = Song.create(title: "Policy of Truth")
-#       visit song_path(song)
-#       expect(page).to have_link("Add Artist", href: edit_song_path(song))
-#     end
+    it "links to edit when no artist" do
+      song = Song.create(title: "Policy of Truth")
+      visit song_path(song)
+      expect(page).to have_link("Add Artist", href: edit_song_path(song))
+    end
 
-#   end
+  end
 
-#   describe "/songs" do
+  describe "/songs" do
 
-#     it "links to the song" do
-#       visit songs_path
-#       expect(page).to have_link("The Grid", href: song_path(@song))
-#     end
+    it "links to the song" do
+      visit songs_path
+      expect(page).to have_link("The Grid", href: song_path(@song))
+    end
 
-#     it "has a link to edit the song if no artist" do
-#       song = Song.create(title: "Mambo No. 5")
-#       visit songs_path
-#       expect(page).to have_link("Add Artist", href: edit_song_path(song))
-#     end
+    it "has a link to edit the song if no artist" do
+      song = Song.create(title: "Mambo No. 5")
+      visit songs_path
+      expect(page).to have_link("Add Artist", href: edit_song_path(song))
+    end
 
-#   end
-# end
+  end
+end
